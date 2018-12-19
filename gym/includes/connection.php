855f563a1fd76c21c4b5a1bc101b03791f694b0c
vuongpn <32337394+vuongpn@users.noreply.github.com>
2018-12-19 23:33:06 +0700

    Upload code

---
+++ gym/includes/connection.php
@@ -0,0 +1,70 @@
+<?php
+    //error_reporting(0);
+ 		 ob_start();
+    session_start();
+
+    
+ 	
+ 	header("Content-Type: text/html;charset=UTF-8");
+	
+	
+		if($_SERVER['HTTP_HOST']=="localhost" or $_SERVER['HTTP_HOST']=="172.20.10.3")
+		{	
+			//local  
+
+				 DEFINE ('DB_USER', 'root');
+				 DEFINE ('DB_PASSWORD', '');
+				 DEFINE ('DB_HOST', 'localhost'); //host name depends on server
+				 DEFINE ('DB_NAME', 'book');
+		}
+		else
+		{
+			//local live 
+
+		 	 DEFINE ('DB_USER', 'root');
+			 DEFINE ('DB_PASSWORD', '');
+			 DEFINE ('DB_HOST', 'localhost'); //host name depends on server
+			 DEFINE ('DB_NAME', 'book');
+		}
+
+	
+	$mysqli =mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+	if ($mysqli->connect_errno) 
+	{
+    	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+
+	mysqli_query($mysqli,"SET NAMES 'utf8'");	 
+
+
+
+	//Settings
+	$setting_qry="SELECT * FROM tbl_settings where id='1'";
+    $setting_result=mysqli_query($mysqli,$setting_qry);
+    $settings_details=mysqli_fetch_assoc($setting_result);
+
+    define("APP_NAME",$settings_details['app_name']);
+    define("APP_LOGO",$settings_details['app_logo']);
+
+    define("API_LATEST_LIMIT",$settings_details['api_latest_limit']);
+    define("API_CAT_ORDER_BY",$settings_details['api_cat_order_by']);
+    define("API_CAT_POST_ORDER_BY",$settings_details['api_cat_post_order_by']);
+    define("API_AUTHOR_ORDER_BY",$settings_details['api_author_order_by']);
+    define("API_AUTHOR_POST_ORDER_BY",$settings_details['api_author_post_order_by']);
+
+    //Profile
+    if(isset($_SESSION['id']))
+    {
+    	$profile_qry="SELECT * FROM tbl_admin where id='".$_SESSION['id']."'";
+	    $profile_result=mysqli_query($mysqli,$profile_qry);
+	    $profile_details=mysqli_fetch_assoc($profile_result);
+
+	    define("PROFILE_IMG",$profile_details['image']);
+    }
+    
+	
+ 
+?> 
+	 
+ 
